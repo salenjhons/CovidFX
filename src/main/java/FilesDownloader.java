53ee5e0e64bd67d5bc44ed9b10b480bd90b4f6cd
salen_jhons <33701845+salenjhons@users.noreply.github.com>
2023-04-05 09:34:02 +0300

    Update FilesDownloader.java

--- src/main/java/FilesDownloader.java
+++ src/main/java/FilesDownloader.java
@@ -36,14 +36,9 @@ public class FilesDownloader {
     private HttpClient client = null;
     HttpGet auth=null;
     HttpGet request = null;
-    private final String mainUrl = "https://xn--j1ab.xn--h1ae9a.xn--p1ai/auth/?backurl=%2Fdocs%2Fmaterials%2Fpath%2F%25D0%259C%25D0%25BE%25D0%25BD%25D0%25B8%25D1%2582%25D0%25BE%25D1%2580%25D0%25B8%25D0%25BD%25D0%25B3%2520%25D0%25BA%25D0%25BB%25D1%258E%25D1%2587%25D0%25B5%25D0%25B2%25D1%258B%25D1%2585%2520%25D0%25BF%25D0%25BE%25D0%25BA%25D0%25B0%25D0%25B7%25D0%25B0%25D1%2582%25D0%25B5%25D0%25BB%25D0%25B5%25D0%25B9%2F";
+    private final String mainUrl = "main_url";
 
-    public static final String[] urls = {
-            "https://xn--j1ab.xn--h1ae9a.xn--p1ai/docs/materials/path/%D0%9C%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3%20%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D1%8B%D1%85%20%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9/%D0%93%D0%BE%D1%82%D0%BE%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BE%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%20%D1%81%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D1%84%D0%B5%D0%BA%D1%86%D0%B8%D0%B5%D0%B9/%D0%9E%D0%B1%D0%B5%D1%81%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%D1%83%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2%20%D0%A4%D0%B5%D0%B4%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8%20%D0%BA%D0%BE%D0%B9%D0%BA%D0%B0%D0%BC%D0%B8%20%D0%B4%D0%BB%D1%8F%20COVID-19%20%D0%B2%20%D1%81%D0%BE%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%81%D1%82%D0%B2%D0%B8%D0%B8%20%D1%81%20%D0%BD%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%B2%D0%BE%D0%BC%20%D0%9C%D0%B8%D0%BD%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%B0%20%D0%A0%D0%A4/",
-            "https://xn--j1ab.xn--h1ae9a.xn--p1ai/docs/materials/path/%D0%9C%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3%20%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D1%8B%D1%85%20%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9/%D0%93%D0%BE%D1%82%D0%BE%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BE%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%20%D1%81%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D1%84%D0%B5%D0%BA%D1%86%D0%B8%D0%B5%D0%B9/%D0%9E%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9%20%D0%BC%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3%20%D0%B3%D0%BE%D1%82%D0%BE%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D1%80%D0%B5%D0%B3%D0%B8%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%20%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BE%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%20%D0%B3%D0%BE%D1%81%D0%BF%D0%B8%D1%82%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%BF%D0%BD%D0%B5%D0%B2%D0%BC%D0%BE%D0%BD%D0%B8%D0%B5%D0%B9/",
-            "https://xn--j1ab.xn--h1ae9a.xn--p1ai/docs/materials/path/%D0%9C%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3%20%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D1%8B%D1%85%20%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9/%D0%93%D0%BE%D1%82%D0%BE%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BE%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%20%D1%81%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D1%84%D0%B5%D0%BA%D1%86%D0%B8%D0%B5%D0%B9/%D0%94%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%BB%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%B8%D0%B9%20%D0%B8%20%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%20%D0%B4%D0%BB%D1%8F%20%D0%B4%D0%B8%D0%B0%D0%B3%D0%BD%D0%BE%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20COVID-19/",
-            "https://xn--j1ab.xn--h1ae9a.xn--p1ai/docs/materials/path/%D0%9C%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3%20%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D1%8B%D1%85%20%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9/%D0%93%D0%BE%D1%82%D0%BE%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BE%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%20%D1%81%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D1%84%D0%B5%D0%BA%D1%86%D0%B8%D0%B5%D0%B9/%D0%9F%D0%B5%D1%80%D0%B2%D0%B8%D1%87%D0%BD%D1%8B%D0%B5%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D0%9C%D0%B8%D0%BD%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%B0/"
-    };
+    public static final String[] urls = ["url1", "url2", "url3", "url4"];
 
 
     public FilesDownloader () {
@@ -229,11 +224,11 @@ public class FilesDownloader {
 
         String patterns [] = {
                 //койки
-                "https://xn--j1ab.xn--h1ae9a.xn--p1ai/disk/showFile/\\d{6}/\\?&amp;ncc=1&amp;ts=\\d{10}&amp;filename=[%\\w]*[\\+[%\\w]*]*\\+COVID-19[\\+[%\\w]*]*[\\+\\s\\d\\.]*[%\\w]*\\.xlsx",
+                "https://xn--j1ab.xn/\\d{6}/\\?&amp;ncc=1&amp;ts=\\d{10}&amp;filename=[%\\w]*[\\+[%\\w]*]*\\+COVID-19[\\+[%\\w]*]*[\\+\\s\\d\\.]*[%\\w]*\\.xlsx",
                 // пневмония лабт
-                "https://xn--j1ab.xn--h1ae9a.xn--p1ai/disk/showFile/\\d{6}/\\?&amp;ncc=1&amp;ts=\\d{10}&amp;filename=[%\\w]*[\\+[%\\w]*]*[\\+\\s\\d\\.]*[%\\w]*\\.xlsx",
+                "https://xn--j1ab.xn/\\d{6}/\\?&amp;ncc=1&amp;ts=\\d{10}&amp;filename=[%\\w]*[\\+[%\\w]*]*[\\+\\s\\d\\.]*[%\\w]*\\.xlsx",
                 //отчет кадры
-                "https://xn--j1ab.xn--h1ae9a.xn--p1ai/disk/showFile/\\d{6}/\\?&amp;ncc=1&amp;ts=\\d{10}&amp;filename=2+[\\+[%\\w]*]*[\\+\\s\\d\\.]*[%\\w]*\\.xlsx"
+                "https://xn--j1ab.xn/\\d{6}/\\?&amp;ncc=1&amp;ts=\\d{10}&amp;filename=2+[\\+[%\\w]*]*[\\+\\s\\d\\.]*[%\\w]*\\.xlsx"
 
         };
 
@@ -279,7 +274,7 @@ public class FilesDownloader {
 
 
     private String labtOneWeekAgo(String labt) throws ParseException {
-        String oneWeekAgo = "https://xn--j1ab.xn--h1ae9a.xn--p1ai/docs/materials/path/%D0%9C%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3%20%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D1%8B%D1%85%20%D0%BF%D0%BE%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9/%D0%93%D0%BE%D1%82%D0%BE%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%B7%D0%B4%D1%80%D0%B0%D0%B2%D0%BE%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B5%20%D1%81%20%D0%B1%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D1%84%D0%B5%D0%BA%D1%86%D0%B8%D0%B5%D0%B9/%D0%94%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%BB%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%B8%D0%B9%20%D0%B8%20%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%20%D0%B4%D0%BB%D1%8F%20%D0%B4%D0%B8%D0%B0%D0%B3%D0%BD%D0%BE%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20COVID-19/%D0%90%D1%80%D1%85%D0%B8%D0%B2/%D0%9E%D1%82%D1%87%D0%B5%D1%82%D1%8B%20%D0%B7%D0%B0%20";
+        String oneWeekAgo = "oneWeekAgo";
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate localDate = LocalDate.parse(labt,dtf);
         LocalDate weekBefore =localDate.minusWeeks(1);
